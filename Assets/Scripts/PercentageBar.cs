e31967729753c32d519610641f046712a3bd2450
Sam McKinley <sammckin@uat.edu>
2020-02-24 12:15:41 -0700

    Added files

---
+++ Assets/Scripts/PercentageBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PercentageBar : MonoBehaviour
+{
+    public float Current = 100.0f;
+
+    public float Max = 100.0f;
+
+    public Image BarImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BarImage = gameObject.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float percentFilled = Current / Max;
+        BarImage.fillAmount = percentFilled;
+        if (percentFilled > 0.25)
+        {
+            BarImage.color = Color.green;
+        }
+        else
+        {
+            BarImage.color = Color.red;
+        }
+    }
+}
