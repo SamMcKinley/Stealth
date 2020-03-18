be82d2264862ad6cc9569aeea72b51aa168f9ec4
Sam McKinley <sammckin@uat.edu>
2020-03-17 20:09:07 -0700

    Close to finishing

---
+++ Assets/Scripts/NoiseMaker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseMaker : MonoBehaviour
+{
+    public float m_CurrentNoiseLevel;
+    public float noiseReducMulti;
+
+    private void Update()
+    {
+        if (m_CurrentNoiseLevel > 0)
+        {
+            m_CurrentNoiseLevel -= Time.deltaTime * noiseReducMulti;
+        }
+    }
+    public void makeNoise(float noisetoMake)
+    {
+        m_CurrentNoiseLevel -= noisetoMake;
+    }
+}
---
+++ Assets/Scripts/StateMachines.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachines : MonoBehaviour
+{
+    public enum EAIStates
+    {
+        idle,
+        chase,
+        dead
+    };
+    public EAIStates currentAIState;
+    private float m_health;
+    public Transform target;
+    public float sightCutoffDistance;
+    public float fieldOfViewAngle;
+    public float hearingCutOffDistance;
+    public float hearingRadius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAIState = EAIStates.idle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StateMachine();
+    }
+
+    private void StateMachine()
+    {
+        switch (currentAIState)
+        {
+            case EAIStates.idle:
+                idle();
+                break;
+            case EAIStates.chase:
+                chase();
+                break;
+            case EAIStates.dead:
+                dead();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void reduceHealth(float amountToReduce)
+    {
+        m_health -= amountToReduce;
+        if (checkDead() == true)
+        {
+            currentAIState = EAIStates.dead;
+        }
+    }
+
+    private bool checkDead()
+    {
+        if (m_health > 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+
+
+    private bool canSee()
+    {
+        if (Vector3.Distance(transform.position, target.position) > sightCutoffDistance)
+        {
+            return false;
+        }
+        Vector3 dirToTarget = target.position - transform.position;
+        if (Vector3.Angle(transform.position, dirToTarget) > (fieldOfViewAngle / 2))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private bool canHear()
+    {
+        if ((target.gameObject.GetComponent<NoiseMaker>().m_CurrentNoiseLevel + hearingRadius) < hearingCutOffDistance)
+        { return false; }
+        return true;
+    }
+   
+    
+
+
+    private void dead()
+    {
+
+    }
+
+
+    private void chase()
+    {
+        if(!canHear() && !canSee())
+        {
+            currentAIState = EAIStates.idle;
+        }
+    }
+
+    private void idle()
+    {
+        if (canHear() || canSee())
+        {
+            currentAIState = EAIStates.chase;
+        }
+    }
+}
