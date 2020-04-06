44997c55557f616e611d15ff71f4d983d8ff627c
Sam McKinley <sammckin@uat.edu>
2020-04-06 14:19:35 -0700

    Added and revised files

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,26 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public NoiseMaker noiseMaker;
+
     private Transform tf;
 
     public float rotationSpeed = 1.0f;
 
     public float movementSpeed = 1.0f;
+
+    public float m_health = 10;
     // Start is called before the first frame update
     void Start()
     {
         tf = gameObject.GetComponent<Transform>();
+        noiseMaker = gameObject.GetComponent<NoiseMaker>();
+        GameManager.instance.Player = this.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
+        noiseMaker.m_CurrentNoiseLevel = 0;
         if (Input.GetKey(KeyCode.UpArrow))
         {
             tf.position += tf.right * movementSpeed * Time.deltaTime;
+            noiseMaker.m_CurrentNoiseLevel += 1;
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
@@ -31,6 +37,33 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow))
         {
             tf.Rotate(0, 0, -rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            tf.position -= tf.right * movementSpeed * Time.deltaTime;
+            noiseMaker.m_CurrentNoiseLevel += 1;
+        }
+    }
+
+    public void reduceHealth(float amountToReduce)
+    {
+        m_health -= amountToReduce;
+        if (checkDead() == true)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private bool checkDead()
+    {
+        if (m_health > 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
         }
     }
 
